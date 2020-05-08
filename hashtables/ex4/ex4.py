382db45fd86241e1041c371b67010079d9bbb51c
Anthony Hill <hillan1152@gmail.com>
2020-05-08 14:12:25 -0400

    completed 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,22 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-
+    cache = {}
+    result = []
+    # iterate through list to gather all k/v pairs
+    for num in range(0, len(a)):
+        number = abs(a[num])
+        if number in cache:
+            cache[number] += 1
+            # print("CACHE NUMBER ", cache[number])
+        else:
+            cache[number] = 1
+        # print("CACHE --> ", cache)
+    for key in cache:   
+        # print("key", key)
+        # print("cache key --> ", cache[key])
+        if cache[key] > 1:
+            result.append(key)
     return result
 
 
